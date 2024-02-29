6ab6ec2b761fffdde226b2d5263e345d355a85d5
Shehta2000 <mohamedshehta819@gmail.com>
2024-02-29 20:45:07 +0200

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.blue,
+    );
   }
 }
